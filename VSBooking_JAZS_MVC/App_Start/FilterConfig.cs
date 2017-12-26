614a876ccb4afc92779be89f39cce9f298623bf8
kuroiNOryuu <kuroi_no_ryuu@hotmail.co.jp>
2017-12-26 01:38:46 +0100

    Add project files.

---
+++ VSBooking_JAZS_MVC/App_Start/FilterConfig.cs
@@ -0,0 +1,13 @@
+﻿using System.Web;
+using System.Web.Mvc;
+
+namespace VSBooking_JAZS_MVC
+{
+    public class FilterConfig
+    {
+        public static void RegisterGlobalFilters(GlobalFilterCollection filters)
+        {
+            filters.Add(new HandleErrorAttribute());
+        }
+    }
+}
---
+++ VSBooking_JAZS_MVC/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="VSBooking_JAZS_MVC.MvcApplication" Language="C#" %>
