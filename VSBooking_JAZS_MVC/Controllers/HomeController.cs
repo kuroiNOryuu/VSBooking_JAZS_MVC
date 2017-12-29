57222a89e634ec7461ec022a0d67a7db759f9fac
AoNoOokami <alicia.zangger@gmail.com>
2017-12-29 22:52:04 +0100

    Created new View : ResSummary. To display it, click on "Book this room" in RoomDetails.

--- VSBooking_JAZS_MVC/Controllers/HomeController.cs
+++ VSBooking_JAZS_MVC/Controllers/HomeController.cs
@@ -46,9 +46,9 @@ namespace VSBooking_JAZS_MVC.Controllers
             List<SearchResult> results = new List<SearchResult>();
             List<Picture> pictures = new List<Picture>
             {
-                new Picture { IdRoom = 1, Url = @"~/res/img/img1.jpg" },
-                new Picture { IdRoom = 2, Url = @"~/res/img/img2.jpg" },
-                new Picture { IdRoom = 3, Url = @"~/res/img/img3.jpg" }
+                new Picture { IdPicture = 1, Url = @"~/res/img/img1.jpg" },
+                new Picture { IdPicture = 2, Url = @"~/res/img/img2.jpg" },
+                new Picture { IdPicture = 3, Url = @"~/res/img/img3.jpg" }
             };
 
             results.Add(new SearchResult { IdRoom = 1, Number = 101, Type = 1, Description = "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor" +
@@ -63,7 +63,7 @@ namespace VSBooking_JAZS_MVC.Controllers
                 "sunt in culpa qui officia deserunt mollit anim id est laborum.",
                 Price = 99, HasTV = true, HasHairDryer = true, HotelName = "Octodure", Location = "Martigny", Pictures = pictures });
 
-            SearchResult result = new ViewModels.SearchResult();
+            SearchResult result = new SearchResult();
             foreach(SearchResult sr in results)
             {
                 if (sr.IdRoom == id)
@@ -76,9 +76,59 @@ namespace VSBooking_JAZS_MVC.Controllers
             return View(result);
         }
 
-        public ActionResult ResSummary()
+        public ActionResult ResSummary(int id)
         {
-            return View();
+            List<SearchResult> rooms = new List<SearchResult>();
+            List<Picture> pictures = new List<Picture>
+            {
+                new Picture { IdRoom = 1, Url = @"~/res/img/img1.jpg" },
+                new Picture { IdRoom = 2, Url = @"~/res/img/img2.jpg" },
+                new Picture { IdRoom = 3, Url = @"~/res/img/img3.jpg" }
+            };
+
+            rooms.Add(new SearchResult
+            {
+                IdRoom = 1,
+                Number = 101,
+                Type = 1,
+                Description = "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor" +
+                " incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. " +
+                "Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, " +
+                "sunt in culpa qui officia deserunt mollit anim id est laborum.",
+                Price = 99,
+                HasTV = true,
+                HasHairDryer = false,
+                HotelName = "Octodure",
+                Location = "Martigny",
+                Pictures = pictures
+            });
+
+            rooms.Add(new SearchResult
+            {
+                IdRoom = 2,
+                Number = 102,
+                Type = 1,
+                Description = "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor" +
+                " incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. " +
+                "Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, " +
+                "sunt in culpa qui officia deserunt mollit anim id est laborum.",
+                Price = 99,
+                HasTV = true,
+                HasHairDryer = true,
+                HotelName = "Octodure",
+                Location = "Martigny",
+                Pictures = pictures
+            });
+
+            List<SearchResult> result = new List<SearchResult>();
+
+            foreach(SearchResult sr in rooms)
+            {
+                if (sr.IdRoom == id)
+                    result.Add(sr);
+            }
+
+            return View(result);
         }
     }
 }
