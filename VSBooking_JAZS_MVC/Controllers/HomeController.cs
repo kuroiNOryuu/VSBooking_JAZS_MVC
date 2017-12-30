24cef2e0ebac0c624ffe6adf42fca60adccc7122
AoNoOokami <alicia.zangger@gmail.com>
2017-12-30 20:36:53 +0100

    Renammed ResSummary in SingleResSummary to differentiate reservation of one romm and reservation of several rooms. Improved SingleResSummary design. When "Cancel" link is clicked, te user goes back to corresponding RoomDetails.

--- VSBooking_JAZS_MVC/Controllers/HomeController.cs
+++ VSBooking_JAZS_MVC/Controllers/HomeController.cs
@@ -12,12 +12,13 @@ namespace VSBooking_JAZS_MVC.Controllers
     public class HomeController : Controller
     {
         
-        
+        /* Search form used as home page */         
         public ActionResult Search()
         {
             return View();
         }
 
+        /* */
         public ActionResult About()
         {
             ViewBag.Message = "Your application description page.";
@@ -25,6 +26,7 @@ namespace VSBooking_JAZS_MVC.Controllers
             return View();
         }
 
+        /* Page displaying the search results */
         public ActionResult SearchResult(Search search)
         {
             List<SearchResult> results = new List<SearchResult>
@@ -36,6 +38,7 @@ namespace VSBooking_JAZS_MVC.Controllers
             return View(results);
         }
 
+        /* Page displaying the details for selected room */
         public ActionResult RoomDetails(int? id)
         {
             if (id == null)
@@ -76,7 +79,8 @@ namespace VSBooking_JAZS_MVC.Controllers
             return View(result);
         }
 
-        public ActionResult ResSummary(int id)
+        /* Page displaying a summary for the reservation of one room */
+        public ActionResult SingleResSummary(int id)
         {
             List<SearchResult> rooms = new List<SearchResult>();
             List<Picture> pictures = new List<Picture>
@@ -120,15 +124,74 @@ namespace VSBooking_JAZS_MVC.Controllers
                 Pictures = pictures
             });
 
-            List<SearchResult> result = new List<SearchResult>();
+            SearchResult result = new SearchResult();
 
             foreach(SearchResult sr in rooms)
             {
                 if (sr.IdRoom == id)
-                    result.Add(sr);
+                    result = sr;
             }
 
             return View(result);
+        }
+
+        /* Page displaying a summary for the reservation of several rooms */
+        public ActionResult ResSummary(List<SearchResult> searchResults)
+        {
+            List<SearchResult> rooms = new List<SearchResult>();
+            List<Picture> pictures = new List<Picture>
+            {
+                new Picture { IdRoom = 1, Url = @"~/res/img/img1.jpg" },
+                new Picture { IdRoom = 2, Url = @"~/res/img/img2.jpg" },
+                new Picture { IdRoom = 3, Url = @"~/res/img/img3.jpg" }
+            };
+
+            rooms.Add(new SearchResult
+            {
+                IdRoom = 1,
+                Number = 101,
+                Type = 1,
+                Description = "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor" +
+                " incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. " +
+                "Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, " +
+                "sunt in culpa qui officia deserunt mollit anim id est laborum.",
+                Price = 99,
+                HasTV = true,
+                HasHairDryer = false,
+                HotelName = "Octodure",
+                Location = "Martigny",
+                Pictures = pictures
+            });
+
+            rooms.Add(new SearchResult
+            {
+                IdRoom = 2,
+                Number = 102,
+                Type = 1,
+                Description = "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor" +
+                " incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. " +
+                "Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, " +
+                "sunt in culpa qui officia deserunt mollit anim id est laborum.",
+                Price = 99,
+                HasTV = true,
+                HasHairDryer = true,
+                HotelName = "Octodure",
+                Location = "Martigny",
+                Pictures = pictures
+            });
+
+            List<SearchResult> results = new List<SearchResult>();
+
+            foreach(SearchResult sr1 in searchResults)
+            {
+                foreach(SearchResult sr2 in rooms)
+                {
+                    if (sr1.IdRoom == sr2.IdRoom)
+                        results.Add(sr2);
+                }
+            }
+
+            return View(results);
         }
     }
 }
